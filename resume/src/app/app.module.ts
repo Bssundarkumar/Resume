5d6553acae3c2497ce8c0511bedc6d98cc5a4db4
Sundar <bssundarkumar@gmail.com>
2017-12-15 12:30:16 -0500

    Resume Home

--- resume/src/app/app.module.ts
+++ resume/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SummaryComponent } from './summary/summary.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SummaryComponent,
+    SkillsComponent,
+    EducationComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
