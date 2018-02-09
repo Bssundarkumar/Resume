851ac74ad5c31feeac1ad22352e662a9cc478389
Sundar <bssundarkumar@gmail.com>
2018-02-09 14:12:51 -0500

    commit

--- resume/src/app/experience/experience.serveice.ts
+++ resume/src/app/experience/experience.serveice.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@angular/core";
-import { Experience } from "./experience.model";
+import { Injectable } from '@angular/core';
+import { Experience } from './experience.model';
 
 
 
 
 @Injectable()
-export class ExperienceService{
+export class ExperienceService {
 
     experience1: string[]= [ 'Developed an SPA for a client using Angular 2/4/5.',
                         'Developed Controller for request, response paradigm by Spring Controllers using Spring-MVC.',
@@ -14,7 +14,6 @@ export class ExperienceService{
                         'Involved in writing queries to retrieve data from the database using DAO Layer.',
                         'Developed Components of the UI using, HTML5, JavaScript, CSS3 and angular 4.',
                         'Fixing QA defect for the application'];
-    
     experience2: string[]= ['Involved in research related to development of the University related application.',
                         'Rewrote the current web app using Angular 4, Typescript, HTML5, CSS3.',
                         'Updated existing websites by altering, removing, or adding content as needed.',
@@ -27,7 +26,7 @@ export class ExperienceService{
                         'Integrating the current work flow with the existing workflow and Testing all the functionalities.',
                         'Involved in developing DAO Layer using Hibernate.',
                         'Implemented transaction management of Relational Databases using MySQL database. '
-                        ]
+                        ];
     experience4: string[]= ['Involved in writing use cases as per the client requirement.',
                         'Developed various business logics in the service layer of the given application.',
                         'Developed UI using HTML5, CSS3 and JavaScript.',
@@ -36,32 +35,36 @@ export class ExperienceService{
         new Experience('Software Engineer Trainee',
                         'Aug 2017 – Dec 2017',
                         'Data Boss International Corp. – New Jersey',
+                        // tslint:disable-next-line:max-line-length
                         'I have developed product distribution module of web application for the Client Globe Pharma related to their products and the product maintenance.',
                         this.experience1,
                         'Spring Boot, Spring, Hibernate, Java/J2EE, HTML5, CSS3, angular JS, MySQL, Eclipse.'),
         new Experience('Java Developer',
                         'Jan 2017 - May 2017',
                         'University of Central Missouri – Missouri',
+                        // tslint:disable-next-line:max-line-length
                         'University of central Missouri is a State university. Here I worked under Prof. who taught Advanced Software Engineering and Software Engineering. Here I developed a project for class registration and class allocation in the Course Advanced Software Engineering.',
                         this.experience2,
                         'Spring, Hibernate, Maven, Eclipse, MySQL, HTML5, CSS3, Git.'),
         new Experience('Systems Engineer',
                        'Sep 2014 - Dec 2015',
                        'Infosys Ltd. – Hyderabad, India.',
+                       // tslint:disable-next-line:max-line-length
                        'Here the client is Medtronic’s.  I involved in developing the modules for the B2B e-commerce web application using Hybris Platform.',
                         this.experience3,
                        'Java/J2EE, Spring, Hibernate, HTML5, CSS3, JSP, Hybris Platform, MS SQL, Eclipse.'),
         new Experience('Jr Java Developer',
                         'May 2013 - July 2014',
                         'Tech matrix compulogics Pvt ltd –Hyderabad, India.',
+                        // tslint:disable-next-line:max-line-length
                         'I have developed product distribution module of web application for the Client Globe Pharma related to their products and the product maintenance.',
                         this.experience4,
                         'Java/J2EE, HTML5, CSS3, JavaScript, Eclipse.')
     ];
-    getExperience(){
+    getExperience() {
         return this.experienceList.slice();
     }
-    getExperienced(id: number){
+    getExperienced(id: number) {
         return this.experienceList[id];
     }
 
