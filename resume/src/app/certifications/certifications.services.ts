d20f82f4926d3576217b0e05a262924bf519c483
Sundar <bssundarkumar@gmail.com>
2018-03-26 12:22:33 -0400

    img

--- resume/src/app/certifications/certifications.services.ts
+++ resume/src/app/certifications/certifications.services.ts
@@ -1,22 +1,22 @@
-import { CertificationsModel } from "./certifications.model";
+import { CertificationsModel } from './certifications.model';
 
-export class CertificaionsServices{
-   private certificatesList:CertificationsModel[]=[
-    new CertificationsModel('Spring and Hibernate','Certificate Of Completion -Spring and Hibernate',
-    'June 2017','https://www.udemy.com/certificate/UC-YS2Z9OUV/','assets/images/Spring-Hibernate.jpg'),
-    new CertificationsModel('Typescript','Certificate Of Completion -Typescript',
-    'Sept 2017','https://www.udemy.com/certificate/UC-JD18N3KS/','assets/images/TypeScript.jpg'),
-    new CertificationsModel('Angular 2','Certificate Of Completion -Angular 2',
+export class CertificaionsServices {
+   private certificatesList: CertificationsModel[]= [
+    new CertificationsModel('Spring and Hibernate', 'Certificate Of Completion -Spring and Hibernate',
+    'June 2017', 'https://www.udemy.com/certificate/UC-YS2Z9OUV/', 'assets/images/Spring-Hibernate.jpg'),
+    new CertificationsModel('Typescript', 'Certificate Of Completion -Typescript',
+    'Sept 2017', 'https://www.udemy.com/certificate/UC-JD18N3KS/', 'assets/images/TypeScript.jpg'),
+    new CertificationsModel('Angular 2', 'Certificate Of Completion -Angular 2',
     'Sept 2017',
     'https://www.udemy.com/certificate/UC-VLNL20SI/',
-    'assets/images/angular 2.jpg')
+    'assets/images/angular 2 full certification.jpg')
 
    ];
 
-   getCertificates(){
+   getCertificates() {
        return this.certificatesList.slice();
    }
-   getCertificate(id:number){
+   getCertificate(id: number) {
     return this.certificatesList[id];
 }
 }
