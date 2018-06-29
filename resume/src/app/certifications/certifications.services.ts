9a6f6e7e0f06d3250707a76b6db8c34da87731a4
Sundar <bssundarkumar@gmail.com>
2018-06-29 01:33:23 -0400

    net

--- resume/src/app/certifications/certifications.services.ts
+++ resume/src/app/certifications/certifications.services.ts
@@ -3,13 +3,13 @@ import { CertificationsModel } from './certifications.model';
 export class CertificaionsServices {
    private certificatesList: CertificationsModel[]= [
     new CertificationsModel('Spring and Hibernate', 'Certificate Of Completion -Spring and Hibernate',
-    'June 2017', 'https://www.udemy.com/certificate/UC-YS2Z9OUV/', 'assets/images/Spring-Hibernate.jpg'),
+    'June 2017', 'https://www.udemy.com/certificate/UC-YS2Z9OUV/', 'assets/images/Spring-Hibernate.gif'),
     new CertificationsModel('Typescript', 'Certificate Of Completion -Typescript',
-    'Sept 2017', 'https://www.udemy.com/certificate/UC-JD18N3KS/', 'assets/images/TypeScript.jpg'),
+    'Sept 2017', 'https://www.udemy.com/certificate/UC-JD18N3KS/', 'assets/images/TypeScript.gif'),
     new CertificationsModel('Angular 2/4/5', 'Certificate Of Completion -Angular 2',
     'Sept 2017',
     'https://www.udemy.com/certificate/UC-VLNL20SI/',
-    'assets/images/angular 2 full certification.jpg')
+    'assets/images/angular-2-full-certificatio.gif')
 
    ];
 
