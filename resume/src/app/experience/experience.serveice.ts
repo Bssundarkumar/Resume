a0bfc42235838cae521397d9d70ea38d0216c642
Sundar <bssundarkumar@gmail.com>
2018-08-28 10:38:44 -0400

    updated experience

--- resume/src/app/experience/experience.serveice.ts
+++ resume/src/app/experience/experience.serveice.ts
@@ -6,10 +6,19 @@ import { Experience } from './experience.model';
 
 @Injectable()
 export class ExperienceService {
-
     // tslint:disable-next-line:max-line-length
-    experience1: string[]= [ 'Involved in creating Single Page Application with loading multiple views using route services and adding more user experience to make it more dynamic by using Angular 2.',
-        'Developed the Model/View using Angular 2/4.',
+    experience4: string[]= ['Involved in creating Single Page Application with loading multiple views using route services and adding more user experience to make it more dynamic by using Angular 2.',
+    'Involved in Developing Visualizations using High chats, High Maps and D3.js.',
+    'Developed REST controllers using Spring REST to serve the UI with required JSON data.',
+    'Used Selectors, Events and Dom Manipulation using components in Angular 2 for updating content on DOM nodes.',
+    'Used ngrx in the angular to get the data from rest api and to manipulate them.',
+    'Used GitHub to maintain the versions of the source code of the project.',
+    'Followed Test Driven Development (TDD) for the components and used Jasmine for unit testing.',
+    'Helping the QA team in testing the application.',
+    'Provided Clarification to a functional requirement to the team for any defect raised.',
+    'Involved in deploy the application in development, test and Production environment.'];
+    // tslint:disable-next-line:max-line-length
+    experience1: string[]= ['Developed the Model/View using Angular 2/4.',
         // tslint:disable-next-line:max-line-length
         'Responsible for Creation of TypeScript reusable components and services to consume REST API’s using Component based architecture provided by Angular 2.',
         'Used Selectors, Events and Dom Manipulation using components in Angular 2 for updating content on DOM nodes.',
@@ -20,11 +29,7 @@ export class ExperienceService {
         'Involved in writing typescript for the components communication in the application.',
         'Implemented RESTful web services using HTTP Client.',
         'Implemented unit testing using Jasmine and Karma.',
-        'Used GitHub to maintain the versions of the source code of the project.',
-        'Followed Test Driven Development (TDD) for the components and used Jasmine for unit testing.',
-        'Helping the QA team in testing the application.',
         'Provided Clarification to a functional requirement to the team for any defect raised.',
-        'Involved in deploy the application in development, test and Production environment.'
         ];
 
     experience2: string[]= [
@@ -62,7 +67,15 @@ export class ExperienceService {
                         'Helped the testing team in testing the applications with different scenarios.'];
     private experienceList: Experience[]= [
         new Experience('Full Stack Developer',
-                        'Aug 2016 – Till Date',
+                        'June 2018 – Till Date',
+                        'Inno-360 – East Brunswick, NJ',
+                        // tslint:disable-next-line:max-line-length
+                        'Here the Project is a web application is a search application with backend Java and frontend using Angular. Here I involved in developing the components of the application and developing charts using highcharts and d3.js',
+                        this.experience4,
+                        // tslint:disable-next-line:max-line-length
+                        'Angular 2/4, TypeScript, Java Spring MVC, High charts, D3.JS, REST API, REST services, Jasmine, Karma, Visual Studio Code, Spring Tool Suite, Apache Solr.'),
+        new Experience('Full Stack Developer',
+                        'Aug 2016 – May 2018',
                         'Orbit Health Care – East Brunswick, NJ',
                         // tslint:disable-next-line:max-line-length
                         'Here the Project is a web application related to Health Care.Here I developed product distribution module of web application for the Client Globe Pharma related to their products and the product maintenance.',
@@ -76,7 +89,7 @@ export class ExperienceService {
                         this.experience2,
                        'Java/J2EE, Spring, Hibernate, HTML5, CSS3, JSP, Hybris Platform, MS SQL, Eclipse.'),
         new Experience('Java Developer',
-                        'Jan 2014 - July 2014',
+                        'May 2014 - July 2014',
                         'Mayukh Soft Solutions –Hyderabad, India.',
                         // tslint:disable-next-line:max-line-length
                         'I have developed product distribution module of web application for the Client Globe Pharma related to their products and the product maintenance.',
